bb3e2ce6310f311b7f74e100e5f952520dbde3cd
Adilson Oliveira <adilsonoj@yahoo.com.br>
2020-08-05 20:25:44 -0300

    initial project

---
+++ src/test/java/br/mil/mar/amrj/dashboardrest/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.mil.mar.amrj.dashboardrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
