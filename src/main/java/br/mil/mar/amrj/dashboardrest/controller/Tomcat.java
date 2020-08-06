8d4c341d8ae0f21951a6f74b30e2dca564ba9b76
Adilson <adilson@amrj.mar.mil.br>
2020-08-06 15:42:42 -0300

    feat(Tomcat): add endpoint list
    list all Applications

---
+++ src/main/java/br/mil/mar/amrj/dashboardrest/controller/Tomcat.java
@@ -0,0 +1,30 @@
+package br.mil.mar.amrj.dashboardrest.controller;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.Base64Utils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import reactor.core.publisher.Flux;
+
+@RestController
+@RequestMapping(value = "/tomcat")
+public class Tomcat {
+
+	@GetMapping("list")
+	public ResponseEntity<Flux<String>> list() {
+		WebClient webClient = WebClient.builder().baseUrl("http://10.1.32.181:8080")
+				.defaultHeader(HttpHeaders.USER_AGENT, "Tomcat").build();
+
+		Flux<String> bodyToFlux = webClient.get().uri("/manager/text/list")
+				.header("Authorization", "Basic " + Base64Utils.encodeToString(("admin" + ":" + "tomcat").getBytes()))
+				.retrieve()
+				.bodyToFlux(String.class);
+
+		return ResponseEntity.ok(bodyToFlux);
+
+	}
+}
